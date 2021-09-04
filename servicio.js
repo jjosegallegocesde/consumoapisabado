9a228b5533738ed6133e2d9fd1fc0497e634267a
jjosegallegocesde <jjosegallegomesa@gmail.com>
2021-09-04 11:56:13 -0500

    se consume api

--- servicio.js
+++ servicio.js
@@ -1,6 +1,6 @@
 let uri="https://api.spotify.com/v1/artists/3YcBF2ttyueytpXtEzn1Za/top-tracks?market=US";
 
-let token="Bearer BQA0fcg5QUuLiIgICP7gAMcvjiZqXdg69a8D3RG6a3dXr_Dxad6IcFPsA7fdAM2O48Cc2X_cloNg8wETW-79aA1CVdypACifSKZCKIQm62xIip0iaJkogt_hvHVTwTlLgAj2fRxwd72uN-R-YZ4";
+let token="Bearer BQAVq3YkLjme3CuVm7E6rsjYQAU48mlgC7i9bhRddGtkkSgyGEbfONcmjTI2zzGOegFm6tc6hOkLWqiQ_1x29Ytb6MRTbp8C_yqvJ2umNSq0Bvg1MsgumQgAYGnKzKM7dpzOBjQPf5RXRGCBG8w";
 
 let parametrosPeticion={
     method:"GET",
@@ -14,8 +14,25 @@ fetch(uri,parametrosPeticion)
     return(respuesta.json());
 })
 .then(function(respuesta){
-    console.log(respuesta);
+    console.log(respuesta); //objeto
+    pintarDatos(respuesta.tracks);
+    
+    /*console.log(respuesta.tracks); //arreglo
+    console.log(respuesta.tracks[0]);//objeto
+    console.log(respuesta.tracks[0].name);
+    console.log(respuesta.tracks[0].preview_url);
+    console.log(respuesta.tracks[0].album.images[0].url);*/
+
 })
+
 .catch(function(error){
     console.log(error);
 })
+
+function pintarDatos(datos){
+
+    datos.forEach(function(cancion){
+        console.log("el lider es el verde")
+    })
+
+}
